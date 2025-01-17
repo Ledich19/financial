d82b30dead81d75592b3a3ea3fad10a63c5c6c56
Aleksandr <crm.31885@gmail.com>
2025-01-17 22:08:41 +0200

    refactor: header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
 
   return (
     <div className={s.header}>
-      <ul className="menu menu-vertical lg:menu-horizontal bg-base-200 rounded-box">
+      <ul className="menu menu-vertical lg:menu-horizontal bg-base-200 rounded-box w-[100%]">
 
         <li><Link to="investment-calculator">InvestmentCalculator</Link></li>
 
