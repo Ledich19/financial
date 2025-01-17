4631ac32b47d8202884a7dd98d33fded81e11e12
Aleksandr <crm.31885@gmail.com>
2025-01-17 21:23:33 +0200

    fix: fix eploing

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import string from 'vite-plugin-string';
+import string from "vite-plugin-string";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/fractal_of_sierpinski/",
-  plugins: [react(), string() ],
+  base: "/financial/",
+  plugins: [react(), string()],
 });
