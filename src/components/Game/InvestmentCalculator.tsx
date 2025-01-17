19a1349678a8522f1155df9287828b34d5b0a470
Aleksandr <crm.31885@gmail.com>
2025-01-17 21:05:33 +0200

    refactor: delete useless

--- src/components/Game/InvestmentCalculator.tsx
+++ src/components/Game/InvestmentCalculator.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 const InvestmentCalculator = () => {
   const [initialInvestment, setInitialInvestment] = useState(1000); // Початкова сума
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,7 @@
-import { useEffect, useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import s from "./header.module.scss"
 
 const Header = () => {
-  const [version, setVersion] = useState('');
-  const location = useLocation();
-
-  useEffect(() => {
-    const params = new URLSearchParams(location.search);
-    const versionParam = params.get('version');
-    if (versionParam) {
-      setVersion(versionParam);
-    }
-  }, [location]);
 
   return (
     <div className={s.header}>
